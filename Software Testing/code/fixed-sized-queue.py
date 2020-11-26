7a3de6f5e3b4a94d92d2fbbdb6f8979fdd701814
Nahid-Hassan <nahid.cseru@gmail.com>
2020-11-26 13:32:49 +0600

    added fixed-sized-queuq.py

---
+++ Software Testing/code/fixed-sized-queue.py
@@ -0,0 +1,50 @@
+import array
+import random
+
+
+class Queue:
+
+    def __init__(self, size_max):
+        assert size_max > 0
+        self.max = size_max
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+        self.data = array.array('i', range(size_max))
+
+    def empty(self):
+        return self.size == 0
+
+    def full(self):
+        return self.size == self.max
+
+    def enqueue(self, x):
+        if self.size == self.max:
+            return False
+        self.data[self.tail] = x
+        self.size += 1
+        self.tail += 1
+        if self.tail == self.max:
+            self.tail = 0
+        return True
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        x = self.data[self.head]
+        self.size -= 1
+        self.head += 1
+        if self.head == self.max:
+            self.head = 0
+        return x
+
+
+q = Queue(2)
+a1 = q.enqueue(6)
+a2 = q.enqueue(7)
+a3 = q.enqueue(8)
+a4 = q.dequeue()
+a5 = q.dequeue()
+a6 = q.dequeue()
+
+print(a1, a2, a3, a4, a5, a6)
