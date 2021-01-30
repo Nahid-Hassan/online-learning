a9880523b893f2c8aacc9f8a3579ea1d7cbd3319
Nahid-Hassan <nahid.cseru@gmail.com>
2021-01-30 11:02:56 +0600

    intro to javascript - loop

---
+++ Intro to JavaScript/code/conditional.js
@@ -0,0 +1,8 @@
+price = 15.00; // price of our hammer
+var money = 20.00; // how much money i have
+
+if (money >= price) {
+    console.log("buy the hammer");
+} else {
+    console.log("don't buy the hammer");
+}
---
+++ Intro to JavaScript/code/switch.js
@@ -0,0 +1,47 @@
+// ex#1
+option  = 1;
+switch (option) {
+  case 1:
+    console.log("You selected option 1.");
+    break;
+  case 2:
+    console.log("You selected option 2.");
+    break;
+  case 3:
+    console.log("You selected option 3.");
+    break;
+  case 4:
+    console.log("You selected option 4.");
+    break;
+  case 5:
+    console.log("You selected option 5.");
+    break;
+  case 6:
+    console.log("You selected option 6.");
+}
+
+
+// ex#2
+var month = 2;
+
+switch(month) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    days = 31;
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    days = 30;
+    break;
+  case 2:
+    days = 28;
+}
+
+console.log("There are " + days + " days in this month.");
