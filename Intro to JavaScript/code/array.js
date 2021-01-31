1f19cc6fc02e54081c707de930874b85d7b4002a
Nahid-Hassan <nahid.cseru@gmail.com>
2021-01-31 14:59:21 +0600

    js array cover

---
+++ Intro to JavaScript/code/array.js
@@ -0,0 +1,17 @@
+// declare an array
+var names = ['nahid', 'hassan', 'mony'];
+console.log(names[4]);
+
+// array length
+console.log(names.length);
+
+// push()
+var nums = [1,2,3];
+nums.push(4);
+console.log(nums);
+
+// pop()
+nums.pop();
+console.log(nums);
+nums.pop();
+console.log(nums);
